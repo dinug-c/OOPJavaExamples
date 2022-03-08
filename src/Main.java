60e812a46e91912c2ab8817832a27f882969b308
Resma Adi Nugroho <resmaadi@gmail.com>
2022-03-09 05:47:37 +0700

    Added Class

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,21 @@ public class Main {
         Kucing.beratHewan(2);
         Kucing.jumlahKakiHewan(4);
         Kucing.cetakHewan();
+
+        Hewan kucing = new Hewan(2);
+        kucing.makan();
+        kucing.jalan();
+        kucing.lari();
+        System.out.println("Umurnya adalah " + kucing.getUmur());
+        
+        // Perhitungan indeks massa tubuh (BMI)
+        // berat(kg) / ( tinggi(m) * tinggi(m) )
+        double bmi = kucing.getBerat() / ((kucing.getTinggi() * 0.01) * (kucing.getTinggi() * 0.01));
+        System.out.println("Indeks massa tubuhnya adalah " + bmi);
+    
+        KelasA kelasA = new KelasA();
+        System.out.println(kelasA.functionB());
+        System.out.println(kelasA.memberB);
+        System.out.println(kelasA.memberC);
     }
 }
